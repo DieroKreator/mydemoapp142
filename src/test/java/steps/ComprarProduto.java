74e6594b5a401c71994d82c74abaf8fbb329cd0a
DieroKreator <qtechwired@gmail.com>
2024-12-10 21:06:03 -0300

    Added method to scroll up

--- src/test/java/steps/ComprarProduto.java
+++ src/test/java/steps/ComprarProduto.java
@@ -4,6 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
+
+import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
@@ -27,6 +33,21 @@ public class ComprarProduto {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void arrastaParaCima(Integer xInicio, Integer yInicio, Integer xFim, Integer yFim) {
+        // Arrastar para cima
+        final var finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        var start = new Point(xInicio, yInicio);
+        var end = new Point(xFim, yFim);
+        var swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0),
+                PointerInput.Origin.viewport(), start.getX(), start.getY()));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000),
+                PointerInput.Origin.viewport(), end.getX(), end.getY()));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        driver.perform(Arrays.asList(swipe));
     }
 
     @Before
